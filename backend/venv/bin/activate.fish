16ecd9c378ed2f1d1fde9dea3bc365fe94d1578f
Maggie Dong <maggiedong@wifi-10-41-95-98.wifi.berkeley.edu>
2025-10-25 18:15:22 -0700

    Myer-briggs

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ianalin/Desktop/few-word-do-trick/backend/venv"
+set -gx VIRTUAL_ENV "/Users/maggiedong/Desktop/projects/few-word-do-trick/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
